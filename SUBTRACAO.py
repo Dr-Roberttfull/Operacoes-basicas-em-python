4a49cf583c824d02181ce3133ce0e705c5c3ed05
FRANCISCO ROBERTO DE SOUSA SILVA <54445149+Dr-Roberttfull@users.noreply.github.com>
2022-02-18 21:57:52 -0300

    Add files via upload
    #Faça um programa que faça a diferença de 2 notas de alunos e imprima na tela
    
    nota1 = int(input("\nDigite a 1ª nota: "))
    nota2 = int(input("\nDigite a 2ª nota: "))
    
    total = nota1 - nota2
    
    print(f'\nA diferença da subtração da nota1 {nota1} e da nota 2 {nota2} é = ',total)
    # O \n  é para saltar uma linha

---
+++ SUBTRACAO.py
@@ -0,0 +1,9 @@
+#Faça um programa que faça a diferença de 2 notas de alunos e imprima na tela
+
+nota1 = int(input("\nDigite a 1ª nota: "))
+nota2 = int(input("\nDigite a 2ª nota: "))
+
+total = nota1 - nota2
+
+print(f'\nA diferença da subtração da nota1 {nota1} e da nota 2 {nota2} é = ',total)
+# O \n  é para saltar uma linha
