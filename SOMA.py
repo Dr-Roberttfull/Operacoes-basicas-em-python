a911cbbcb874446b0a148129101748f51216da15
FRANCISCO ROBERTO DE SOUSA SILVA <54445149+Dr-Roberttfull@users.noreply.github.com>
2022-02-18 21:56:57 -0300

    Update SOMA.py

--- SOMA.py
+++ SOMA.py
@@ -1,8 +1,9 @@
 #Faça um programa que some 2 notas de alunos e imprima na tela
 
 nota1 = int(input("\nDigite a 1ª nota: "))
-
 nota2 = int(input("\nDigite a 2ª nota: "))
+
 soma = nota1 + nota2
+
 print(f'\nO resultado soma da nota1 {nota1} com a nota 2 {nota2} é = ',soma)
 # O \n  é para saltar uma linha
