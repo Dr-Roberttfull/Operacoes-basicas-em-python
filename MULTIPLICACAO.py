241f55a29c93c6d156998804b4d52f13c3d284b9
FRANCISCO ROBERTO DE SOUSA SILVA <54445149+Dr-Roberttfull@users.noreply.github.com>
2022-02-18 22:03:33 -0300

    Add files via upload
    #Faça um programa que faça a multplicaçã de 2 números e imprima na tela
    
    num1 = int(input("\nDigite o 1º número: "))
    num2 = int(input("\nDigite o 2º número: "))
    
    total = num1 * num2
    
    print(f'\nO produto da multiplicaçã numero {num1} com o número {num2} é = ',total)
    print('')
    # O \n  é para saltar uma linha
    # O print('') é para saltar uma linha no final do código

---
+++ MULTIPLICACAO.py
@@ -0,0 +1,11 @@
+#Faça um programa que faça a multplicaçã de 2 números e imprima na tela
+
+num1 = int(input("\nDigite o 1º número: "))
+num2 = int(input("\nDigite o 2º número: "))
+
+total = num1 * num2
+
+print(f'\nO produto da multiplicaçã numero {num1} com o número {num2} é = ',total)
+print('')
+# O \n  é para saltar uma linha
+# O print('') é para saltar uma linha no final do código
