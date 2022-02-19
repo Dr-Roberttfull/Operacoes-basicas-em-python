406bd04706e766f148b1a3490d3959da22eab563
FRANCISCO ROBERTO DE SOUSA SILVA <54445149+Dr-Roberttfull@users.noreply.github.com>
2022-02-18 21:51:51 -0300

    Add files via upload
    #Faça um programa que some 2 notas de alunos e imprima na tela
    
    nota1 = int(input("\nDigite a 1ª nota: "))
    
    nota2 = int(input("\nDigite a 2ª nota: "))
    soma = nota1 + nota2
    print(f'\nO resultado soma nota1 {nota1} e da nota 2 {nota2} é = ',soma)
    # O \n  é para saltar uma linha

---
+++ SOMA.py
@@ -0,0 +1,8 @@
+#Faça um programa que some 2 notas de alunos e imprima na tela
+
+nota1 = int(input("\nDigite a 1ª nota: "))
+
+nota2 = int(input("\nDigite a 2ª nota: "))
+soma = nota1 + nota2
+print(f'\nO resultado soma nota1 {nota1} e da nota 2 {nota2} é = ',soma)10
+# O \n  é para saltar uma linha
